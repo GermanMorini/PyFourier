cf84b12a8015a14e36f33833d0feb274abc0b0cd
GermanMorini <germorini25@gmail.com>
2025-02-15 13:57:34 -0300

    mejorado el ejemplo de reconstruccion del sonido

--- data_to_sound.py
+++ data_to_sound.py
@@ -4,8 +4,8 @@ import pandas as pd
 from scipy.io.wavfile import write
 from sys import argv
 
-if len(argv) != 3:
-    print(f"Uso: {argv[0]} ARCHIVO.csv DEC_TYPE")
+if len(argv) != 5:
+    print(f"Uso: {argv[0]} ARCHIVO.csv DEC_TYPE DURACION DEC_TIME")
     exit(1)
 
 dec_type = {
@@ -15,10 +15,10 @@ dec_type = {
 }
 
 archivo_csv = argv[1]
-duracion = 2.0
+duracion = float(argv[3])
 tasa_muestreo = 44100
 decay_type = dec_type[int(argv[2])]
-decay_time = 0.3
+decay_time = float(argv[4])
 nombre_archivo_salida = f"{archivo_csv}-reconstruido.wav"
 
 try:
